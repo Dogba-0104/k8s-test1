bc1d11f56edb68b233b4ecffebf1addc305c030f
john power <john@johns-MacBook-Pro.local>
2022-07-23 13:09:11 +0100

    fixing verify

--- scenario1/step2/verify.sh
+++ scenario1/step2/verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-if [ `k get pod nginx -o jsonpath='{.status.phase}'` = 'Running']
+if [ $(k get pod nginx -o jsonpath='{.status.phase}') = 'Running' ]
 echo 'You have fixed it here is the flag adshgfahgdfluralgiewiashdgfhgfriay245602' 
