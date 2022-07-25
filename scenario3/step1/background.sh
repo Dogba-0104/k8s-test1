898c50a2d8b1b62805a6950eba76b0e47f447cc0
john power <john@johns-MacBook-Pro.local>
2022-07-25 01:02:16 +0100

    fixing scenario3

--- scenario3/step1/background.sh
+++ scenario3/step1/background.sh
@@ -2,28 +2,6 @@
 
 echo "Setting things up ....."
 
-cat <<EOF | kubectl apply -f -                                                                                                                         0|1 х │ 4s 
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  labels:
-    app: nginx
-  name: nginx
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      nodeName: worker1
-      containers:
-      - image: nginx
-        name: nginx
-EOF
-
+kubectl create -f deploy.yaml
 
 sleep 10
